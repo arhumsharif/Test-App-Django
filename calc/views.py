356dece6618f145c6382e4a23f3ec4902ba3b9aa
Muhammad Arhum <arhumsharif06@gmail.com>
2024-03-27 23:06:04 +0500

    Adding two numbers with a get request

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Hello World!')
+
+def addNumbers(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    result = val1 + val2
+    return HttpResponse('Result: ' + str(result))
